9818d16ddbf2b543c2d374537e3d2aab0fd9b49c
Kholmurodov <suhrobkholmurodov1@gmail.com>
2025-05-03 23:42:53 +0500

    fix: localization for mode switcher

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,16 +1,18 @@
-import { Cog, Moon, Sun } from "lucide-react"
+import { Cog, Moon, Sun } from 'lucide-react';
 
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/components/theme-provider"
+} from '@/components/ui/dropdown-menu';
+import { useTheme } from '@/components/theme-provider';
+import { useLocalization } from '@/hooks/useLocalization';
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme } = useTheme();
+  const { t } = useLocalization();
 
   return (
     <DropdownMenu>
@@ -18,20 +20,22 @@ export function ModeToggle() {
         <Button variant="outline" className="hover:cursor-pointer" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem className="hover:cursor-pointer" onClick={() => setTheme("light")}>
-          <Sun /> Light
+        <DropdownMenuItem className="hover:cursor-pointer" onClick={() => setTheme('light')}>
+          <Sun />
+          {t('switcher.light')}
         </DropdownMenuItem>
-        <DropdownMenuItem className="hover:cursor-pointer" onClick={() => setTheme("dark")}>
-         <Moon />  Dark
+        <DropdownMenuItem className="hover:cursor-pointer" onClick={() => setTheme('dark')}>
+          <Moon />
+          {t('switcher.dark')}
         </DropdownMenuItem>
-        <DropdownMenuItem className="hover:cursor-pointer" onClick={() => setTheme("system")}>
-          <Cog /> System
+        <DropdownMenuItem className="hover:cursor-pointer" onClick={() => setTheme('system')}>
+          <Cog />
+          {t('switcher.system')}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
